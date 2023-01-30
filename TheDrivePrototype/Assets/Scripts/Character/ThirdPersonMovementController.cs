08ee822a16da5e057df681264e97adf32f0a14ce
gabbag00l <owenhmarshall11@gmail.com>
2023-01-30 18:04:33 +0000

    player controller

--- TheDrivePrototype/Assets/Scripts/Character/ThirdPersonMovementController.cs
+++ TheDrivePrototype/Assets/Scripts/Character/ThirdPersonMovementController.cs
@@ -9,6 +9,17 @@ public class ThirdPersonMovementController : MonoBehaviour
       public float turnSmoothTime = 0.1f;
       float turnSmoothVelocity;
 
+    public Animator animator;
+
+    public Transform cam;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayer;
+    Vector3 velocity;
+    bool isGrounded;
+    public float gravity = -9.81f;
+
       // Update is called once per frame
       void Update()
       {
@@ -16,6 +27,19 @@ public class ThirdPersonMovementController : MonoBehaviour
         {
             return;
         }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+         
         
         float horizontal = Input.GetAxisRaw("Horizontal");
           float vertical = Input.GetAxisRaw("Vertical");
@@ -23,12 +47,20 @@ public class ThirdPersonMovementController : MonoBehaviour
           Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
           if (direction.magnitude >= 0.1f)
               {
-              float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+              float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
               float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
               transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-              characterController.Move(direction * speed * Time.deltaTime);
+              Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+              characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
+              animator.SetBool("animatorIsRunning", true);
               }
+
+          else
+        {
+            animator.SetBool("animatorIsRunning", false);
+        }
+
       }
 
       
